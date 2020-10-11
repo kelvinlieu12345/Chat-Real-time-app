ed7b81fc01eb57602cc6a656539c2e1d6b623d18
Kelvin Lieu <kelvinlieu12345@gmail.com>
2020-10-11 09:23:59 +0800

    first commit

---
+++ Chat/Place/ChatingPlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+namespace Chat.Place
+{
+    public class ChatingPlace : Hub
+    {
+        public async Task SdMsg(string name, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMsg", name, msg);
+        }
+    }
+}
---
+++ Chat/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var conn = new signalR.HubConnectionBuilder().withUrl("/chattingplace").build();
+
+document.getElementById("sdBtn").disabled = true;
+
+conn.on("ReceiveMsg", function (name, msg) {
+    var m = msg.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+    var eM = name + " added: " + m;
+    var li = document.createElement("li");
+    li.textContent = eM;
+    document.getElementById("msgList").append(li);
+
+});
+
+conn.start().then(function () {
+    document.getElementById("sdBtn").disabled = false;
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sdBtn").addEventListener("click", function (event) {
+    var name = document.getElementById("name").value;
+    var msg = document.getElementById("msg").value;
+    conn.invoke("SdMsg", name, msg).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
